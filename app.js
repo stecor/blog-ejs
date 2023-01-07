2f3f0fb05cebef31419e0d4d786c58f001e81999
Stefano Corra <stefano.corra@hotmail.com>
2023-01-07 16:33:14 -0500

    home commit

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const homeStartContent = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book. It has survived not only five centuries, but also the leap into
+electronic typesetting, remaining essentially unchanged. It was popularised in
+the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker
+including versions of Lorem Ipsum.`
+
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
@@ -10,7 +19,7 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-  res.render('home')
+  res.render('home', { StartContent: homeStartContent })
 })
 
 app.listen(3000, function () {
