64c49392c1f443d63b8d79815c9628ee286e1d27
Stefano Corra <stefano.corra@hotmail.com>
2023-01-15 15:55:24 -0500

    post commit

--- app.js
+++ app.js
@@ -63,6 +63,10 @@ app.post('/compose', function (req, res) {
   res.redirect('/')
 })
 
+app.get('/posts/:postName', function (req, res) {
+  console.log(req.params.postName)
+})
+
 app.listen(3000, function () {
   console.log('Server running port 3000')
 })
